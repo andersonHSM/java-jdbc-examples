d7414c9a43ebe04cc8644518895704bd7ced6a7a
Anderson Menezes <andersonmenezes.dev@gmail.com>
2023-11-23 07:46:20 -0300

    basic reading from resultSet

--- jdbc1/src/main/java/application/Main.java
+++ jdbc1/src/main/java/application/Main.java
@@ -3,10 +3,31 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        connection = DB.getConnection();
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM department");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Id")+ " | " + resultSet.getString( "Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResultSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
+
     }
 }
--- jdbc1/src/main/java/db/DB.java
+++ jdbc1/src/main/java/db/DB.java
@@ -3,9 +3,7 @@ package db;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -36,6 +34,25 @@ public class DB {
             }
         }
     }
+
+    public static void closeStatement(Statement statement) {
+        try {
+            statement.close();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
+    }
+
+    public static void closeResultSet(ResultSet resultSet) {
+        try {
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
+    }
+
 
     private static Properties loadProperties() {
         try (FileInputStream fs = new FileInputStream("db.properties")) {
